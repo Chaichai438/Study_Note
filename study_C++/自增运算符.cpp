1f591616a24c454efdf78385c3185ac51f642c9a
Chaichai438 <144552092+Chaichai438@users.noreply.github.com>
2024-11-18 14:05:14 +0800

    Add files via upload

---
+++ study_C++/自增运算符.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    a++;
+    b--;
+
+    cout << a << endl;
+    cout << b << endl;
+
+    return 0;
+}

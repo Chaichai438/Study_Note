9ccf49853371770ee24b9281a5745efc10487f18
Chaichai438 <144552092+Chaichai438@users.noreply.github.com>
2024-11-18 18:37:31 +0800

    Add files via upload

---
+++ study_C++/1.helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;      //使用std的命名空间，main主题中就不需要再加入std::
+int main()
+{
+   
+    cout << "Hello World!!!" << "\n";
+    cout << "This is a C++ program." << "\n";// “\n" = endl
+
+    return 0;
+}
+/* 
+     cout的具体含义：
+     用于向标准输出流（通常是屏幕）输出数据
+     相当于printf
+*/
+
